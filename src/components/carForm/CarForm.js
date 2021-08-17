ad3db94d72181cdf4af57c5e5ad033e82d8ce30e
GvkBogdan <hvbogdan777@gmail.com>
2021-08-17 13:15:27 +0300

    Class-Work-React-3

---
+++ src/components/carForm/CarForm.js
@@ -0,0 +1,57 @@
+import {getCars, saveCar} from "../../services/car.api.service";
+import {useEffect, useState} from "react";
+import Car from "../car/Car";
+import './CarForm.css'
+
+
+export default function CarForm() {
+
+  // let [model, setModel] = useState('enter model')
+  // let [price, setPrice] = useState('enter price')
+  // let [year, setYear] = useState('enter year')
+
+    let onFormSubmit = (e) =>{
+      e.preventDefault()
+      // console.log(e.target.id.value);
+      // console.log(e.target.model.value);
+      // console.log(e.target.price.value);
+      // console.log(e.target.year.value);
+
+      let carSave = {model: e.target.model.value,price: e.target.price.value, year: e.target.year.value};
+      console.log(carSave)
+
+        saveCar(carSave);
+
+    };
+
+
+    let[cars, setCars] = useState([])
+
+    useEffect(() => {
+        getCars().then(value => setCars([...value]))
+    }, [])
+
+
+    return (
+        <div>
+            <div className={'form-car'}>
+                <form onSubmit={onFormSubmit}>
+                    <input type="text" name={'model'} placeholder={'enter model'}/>
+                    <input type="text" name={'price'} placeholder={'enter price'}/>
+                    <input type="text" name={'year'} placeholder={'enter year'}/>
+                    <button>save</button>
+                </form>
+            </div>
+
+
+            <div className={'car-box'}>
+
+                {
+                    cars.map(value => <Car car={value} key={value.id}/>)
+                }
+
+            </div>
+
+        </div>
+    );
+}
