e7c8bdd2280765fe121c80a403a031d35172774e
GvkBogdan <hvbogdan777@gmail.com>
2021-08-17 16:21:36 +0300

    Home-Work-React-4 + other methods

--- src/components/superform/SuperForm.js
+++ src/components/superform/SuperForm.js
@@ -1,7 +1,9 @@
-import {useState} from "react";
+import {createRef, useState} from "react";
 import {saveCar} from "../../services/car.api.service";
 
 export default function SuperForm() {
+
+   // let refObject = createRef();// спосіб-3.... ще одна можливість отримувати дані з форми
 
     let[formState, setFormState] = useState({model:'', price:'', year:''})
 
@@ -13,13 +15,26 @@ export default function SuperForm() {
         e.preventDefault();
         console.log(formState);
         saveCar(formState);
-    }
+
+        // Спосіб-3
+
+        //в формі прописати ref={refObject}
+        /*console.log(refObject.current.model.value);// отримуємо дані
+        console.log(refObject.current.price.value);// отримуємо дані
+        console.log(refObject.current.year.value);// отримуємо дані
+        let m = refObject.current.model.value;
+        let p = refObject.current.price.value;
+        let y = refObject.current.year.value;
+        saveCar({model:m, price:p, year:y})
+         */
+
+    };
 
 
     return (
         <div>
 
-            <div className={'form-car'}>
+            <div className={'form-car'} >
                 <form onSubmit={saveC}>
                     <input type="text" name={'model'} value={formState.model}
                            onChange={onFormChange}/>
